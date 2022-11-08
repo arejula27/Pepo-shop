8567097d595e1f310397530876b561596ecc08c2
Íñigo Aréjula Aísa <arejula27@gmail.com>
2022-11-08 11:12:10 +0100

    intsakacion mui y db

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@emotion/react";
+import { lightTheme } from "../themes/light-theme";
+import { CssBaseline } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
